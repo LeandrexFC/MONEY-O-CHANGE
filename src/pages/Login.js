785e61184fde78d2e821cf66431719efa0b49b15
LeandreXFC <leandro.sousa23tec@gmail.com>
2022-12-08 19:14:33 -0300

    Req 1 concluido

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,9 +1,78 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import { addEmail } from '../redux/actions';
 
 class Login extends React.Component {
+  state = {
+    email: '',
+    password: '',
+  };
+
+  onInputchange = (event) => {
+    const { name, value } = event.target;
+    this.setState({
+      [name]: value,
+    });
+  };
+
+  validateButton = () => {
+    const { email, password } = this.state;
+
+    const correctForm = email.includes('@' && '.com');
+    const number = 6;
+    const minCaratcter = password.length >= number;
+
+    return correctForm && minCaratcter;
+  };
+
+  attReducerAndRedirect = (event) => {
+    event.preventDefault();
+    const { history, dispatch } = this.props;
+    const { email } = this.state;
+
+    dispatch(addEmail(email));
+
+    history.push('/carteira');
+  };
+
   render() {
-    return <div>Login</div>;
+    return (
+      <form onSubmit={ this.redirectToWallet }>
+        Email:
+        <input
+          name="email"
+          type="email"
+          data-testid="email-input"
+          placeholder="Digite seu Email:"
+          onChange={ this.onInputchange }
+          required
+        />
+        Senha:
+        <input
+          name="password"
+          type="password"
+          data-testid="password-input"
+          placeholder="Digite sua senha:"
+          onChange={ this.onInputchange }
+        />
+        <button
+          type="button"
+          onClick={ this.attReducerAndRedirect }
+          disabled={ !this.validateButton() }
+        >
+          Entrar
+        </button>
+      </form>
+    );
   }
 }
 
-export default Login;
+Login.propTypes = {
+  dispatch: PropTypes.func.isRequired,
+  history: PropTypes.shape({
+    push: PropTypes.func.isRequired,
+  }).isRequired,
+};
+
+export default connect()(Login);
