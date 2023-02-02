bedae6b32a1a0cb7ff096f5eb8cfa55727be0e75
LeandreXFC <leandro.sousa23tec@gmail.com>
2023-02-02 12:14:23 -0300

    Finishing CSS

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,9 +1,13 @@
+/* eslint-disable jsx-a11y/no-noninteractive-element-interactions */
+/* eslint-disable jsx-a11y/click-events-have-key-events */
 /* eslint-disable react/jsx-max-depth */
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import { deleteExpenses, editExpenses, inputsAtt } from '../redux/actions';
 import '../allCss/Table.css';
+import trashImg from '../Img/trash.png';
+import editImg from '../Img/edit.png';
 
 class Table extends Component {
   deleteAllExpenses = (expenseId) => {
@@ -13,6 +17,7 @@ class Table extends Component {
   };
 
   editSomeExpenses = (expense) => {
+    console.log(expense);
     const { dispatch, expenses } = this.props;
 
     const { value, description, currency, method, tag } = expenses[expense];
@@ -34,65 +39,61 @@ class Table extends Component {
     return (
       <div className="allTable">
         <table className="table">
-          <thead className="thead">
-            <tr className="trTable">
-              <th> Descrição </th>
-              <th> Tag </th>
-              <th>
-                Método de
-                <br />
-                pagamento
-              </th>
-              <th> Valor </th>
-              <th> Moeda </th>
-              <th> Câmbio utilizado </th>
-              <th> Valor convertido </th>
-              <th>
-                Moeda de
-                <br />
-                conversão
-              </th>
-              <th> Editar/Excluir </th>
+          <tr className="trTable">
+            <th className="thTable2"> Descrição </th>
+            <th className="thTable2"> Tag </th>
+            <th className="thTable2">
+              Método de
+              <br />
+              pagamento
+            </th>
+            <th className="thTable2"> Valor </th>
+            <th className="thTable2">Moeda </th>
+            <th className="thTable2"> Câmbio utilizado </th>
+            <th className="thTable2"> Valor convertido </th>
+            <th className="thTable2">
+              Moeda de
+              <br />
+              conversão
+            </th>
+            <th className="thTable2 "> Editar/Excluir </th>
+          </tr>
+          {expenses.map((expense) => (
+            <tr key={ expense.id } className="trBody">
+              <td className="tdTable desc">{expense.description}</td>
+              <td className="tdTable">{expense.tag}</td>
+              <td className="tdTable">{expense.method}</td>
+              <td className="tdTable">{(+expense.value).toFixed(2)}</td>
+              <td className="tdTable desc2">
+                {expense.exchangeRates[expense.currency].name.split('/')[0]}
+              </td>
+              <td className="tdTable">
+                {(+expense.exchangeRates[expense.currency].ask).toFixed(2)}
+              </td>
+              <td className="tdTable">
+                {(
+                  +expense.value * expense.exchangeRates[expense.currency].ask
+                ).toFixed(2)}
+              </td>
+              <td className="tdTable">Real</td>
+              <td className="tdTable">
+
+                <img
+                  src={ editImg }
+                  alt="edit Img"
+                  className="btn-edit"
+                  onClick={ () => this.editSomeExpenses(expense.id) }
+                />
+
+                <img
+                  src={ trashImg }
+                  alt="trash Img"
+                  className="btn-edit"
+                  onClick={ () => this.deleteAllExpenses(expense.id) }
+                />
+              </td>
             </tr>
-          </thead>
-          <tbody className="tbody">
-            {expenses.map((expense) => (
-              <tr key={ expense.id } className="trBody">
-                <td className="tdTable">{expense.description}</td>
-                <td className="tdTable">{expense.tag}</td>
-                <td className="tdTable">{expense.method}</td>
-                <td className="tdTable3">{(+expense.value).toFixed(2)}</td>
-                <td className="tdTable">
-                  {expense.exchangeRates[expense.currency].name.split('/')[0]}
-                </td>
-                <td className="tdTable">
-                  {(+expense.exchangeRates[expense.currency].ask).toFixed(2)}
-                </td>
-                <td className="tdTable">
-                  {(
-                    +expense.value * expense.exchangeRates[expense.currency].ask
-                  ).toFixed(2)}
-                </td>
-                <td className="tdTable">Real</td>
-                <td className="tdTable">
-                  <button
-                    type="button"
-                    data-testid="edit-btn"
-                    onClick={ () => this.editSomeExpenses(expense.id) }
-                  >
-                    Editar
-                  </button>
-                  <button
-                    type="button"
-                    data-testid="delete-btn"
-                    onClick={ () => this.deleteAllExpenses(expense.id) }
-                  >
-                    Excluir
-                  </button>
-                </td>
-              </tr>
-            ))}
-          </tbody>
+          ))}
         </table>
       </div>
     );
